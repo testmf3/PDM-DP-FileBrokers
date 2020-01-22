fe4935d279ae99cacbfccdd267f5aaa979a5d0d7
testmf3 <testmf3@archimatika.com>
2020-01-22 23:14:19 +0200

    Created worker

---
+++ Worker/Config.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Worker_001
+{
+    class Config
+    {
+        public int number { get; set; }
+        public string applicationName { get; set; }
+        public void toConfig(Dictionary<String, String> configurationDictionary) {
+
+            number = int.Parse(configurationDictionary["number"]);
+            applicationName = configurationDictionary["applicationName"];
+        }
+
+        public int Sum(int value) {
+            this.number = this.number + value;
+            return this.number;
+        }
+
+        public override string ToString()
+        {
+            return "Name:  " + applicationName   + "\nArgument:  " + number;
+        }
+    }
+}
---
+++ Worker/Program.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace Worker_001
+{
+    class Program
+    {
+
+        public IConfigurationRoot Configuration { get; set; }
+
+        static void Main(string[] args)
+        {
+
+
+            var factory = new ConnectionFactory()
+            {
+                HostName = "94.131.241.80", Port = 5672,
+                UserName = "testmf3", Password = "As123456"
+            };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+
+                        var configurationString = Encoding.Default.GetString(body);
+                        Dictionary<String, String> configurationDictionary = JsonConvert.DeserializeObject<Dictionary<String, String>>(configurationString);
+
+                        Config config = new Config();
+
+                        config.toConfig(configurationDictionary);
+                        Console.WriteLine("To config object");
+                        Console.WriteLine(config);
+
+                        config.Sum(5);
+
+                        Console.WriteLine("To config object");
+                        Console.WriteLine(config);
+                    };
+                    channel.BasicConsume(queue: "hello", autoAck: true, consumer: consumer);
+
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                }
+            }
+        } 
+    } 
+}
+   
