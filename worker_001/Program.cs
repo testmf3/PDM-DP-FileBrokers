303f6b044622698d9f1c3c5ca4048ba7c93e5748
me? <viktoriya.shyyan@gmail.com>
2020-01-27 19:16:47 +0200

    created worker

--- worker_001/Program.cs
+++ worker_001/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using System;
 
 namespace worker_001
 {
@@ -6,7 +7,59 @@ namespace worker_001
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+            var factory = new ConnectionFactory()
+            {
+                HostName = Constant.HOSTNAME,
+                Port = Constant.PORT,
+                UserName = Constant.USERNAME,
+                Password = Constant.PASSWORD
+            };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(
+                        queue: Constant.HOSTNAME,
+                        durable: Constant.DURABLE,
+                        exclusive: Constant.EXCLUSIVE,
+                        autoDelete: Constant.AUTODELETE,
+                        arguments: Constant.ARGUMENTS);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        //Message body
+                        IMapMessageReader messageReader = new MapMessageReader(ea.BasicProperties, ea.Body);
+
+                        Config config = new Config();
+
+                        config.ToConfig(messageReader);
+                        Console.WriteLine("To config object");
+                        Console.WriteLine(config);
+
+                        config.Sum(Constant.NUMBER);
+
+                        Console.WriteLine("To config object after sum");
+                        Console.WriteLine(config);
+
+                        //Unsuscribe
+                        channel.BasicReject(ea.DeliveryTag, false);
+                        Console.WriteLine(" [x] Done");
+
+                    };
+                    channel.BasicConsume(
+                        queue: Constant.QUEUE,
+                        autoAck: Constant.AUTOACK,
+                        consumer: consumer);
+
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                }
+            }
+
         }
     }
 }
